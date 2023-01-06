e185ae27b991e3cdfcef0457f6e163d55293c5b4
Herman Zuiakov <gzuyakov@gmail.com>
2023-01-06 18:40:22 +0100

    Setting up new .env file configuration

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-require('dotenv').config();
+import env from "react-dotenv";
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTS_ID
+    apiKey: env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: env.REACT_APP_FIREBASE_MEASUREMENTS_ID
 };
 
 export const app = initializeApp(firebaseConfig);
